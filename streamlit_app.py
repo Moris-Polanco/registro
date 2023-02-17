5e41dd955d8e8bfe38dbc91da1a2b2bec135fbd7
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-02-17 12:16:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,26 +10,54 @@ c.execute('''CREATE TABLE IF NOT EXISTS users
              (name TEXT, email TEXT, password TEXT)''')
 
 # Título de la página
-st.title("Registro")
+st.title("Registro / Inicio de Sesión")
 
-# Formulario de registro
-name = st.text_input("Nombre completo")
-email = st.text_input("Correo electrónico")
-password = st.text_input("Contraseña", type="password")
-confirm_password = st.text_input("Confirmar contraseña", type="password")
-
-# Botón de registro
+# Página de registro
 if st.button("Registrarse"):
-    # Validación de contraseña
-    if password == confirm_password:
-        # Insertar los datos del usuario en la base de datos
-        c.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", (name, email, password))
-        conn.commit()
-        # Mensaje de confirmación
-        st.success("Te has registrado correctamente")
-    else:
-        # Mensaje de error
-        st.error("Las contraseñas no coinciden")
+    st.subheader("Registro")
+    # Formulario de registro
+    name = st.text_input("Nombre completo")
+    email = st.text_input("Correo electrónico")
+    password = st.text_input("Contraseña", type="password")
+    confirm_password = st.text_input("Confirmar contraseña", type="password")
+    
+    # Botón de registro
+    if st.button("Registrarse"):
+        # Validación de contraseña
+        if password == confirm_password:
+            # Insertar los datos del usuario en la base de datos
+            c.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", (name, email, password))
+            conn.commit()
+            # Mensaje de confirmación
+            st.success("Te has registrado correctamente")
+        else:
+            # Mensaje de error
+            st.error("Las contraseñas no coinciden")
+            
+# Página de inicio de sesión
+if st.button("Iniciar Sesión"):
+    st.subheader("Inicio de Sesión")
+    # Formulario de inicio de sesión
+    email = st.text_input("Correo electrónico")
+    password = st.text_input("Contraseña", type="password")
+    
+    # Botón de inicio de sesión
+    if st.button("Iniciar Sesión"):
+        # Buscar el usuario en la base de datos
+        query = "SELECT * FROM users WHERE email=? AND password=?"
+        result = c.execute(query, (email, password))
+        user = result.fetchone()
         
+        # Validar el inicio de sesión
+        if user is not None:
+            # Mensaje de confirmación
+            st.success("Inicio de sesión exitoso")
+            # Redireccionamiento a la página de evaluador de ensayos
+            st.markdown(f"Redireccionando a https://evaluador-ensayos.streamlit.app")
+            st.experimental_rerun()
+        else:
+            # Mensaje de error
+            st.error("Credenciales incorrectas")
+
 # Cerrar la conexión con la base de datos
 conn.close()
